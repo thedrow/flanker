c29ee1ea3c819aac16eb49d762220afa8ac998ce
Russell Jones <rjones@mailgunhq.com>
2014-01-02 14:36:05 -0800

    Fix.

--- flanker/mime/message/headers/parsing.py
+++ flanker/mime/message/headers/parsing.py
@@ -42,9 +42,6 @@ def parse_header_value(name, val):
                 return ContentType(main, sub, params)
             else:
                 return WithParams(val, params)
-        #elif "=?" in val:
-        #    # may be encoded word
-        #    return encodedword.decode(val)
         else:
             return val
 
